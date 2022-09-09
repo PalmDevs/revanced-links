4d66b302e80c0493cea748848d2ac6dc72158098
Palm <palmpasuthorn@gmail.com>
2022-09-09 19:32:54 +0700

    chore: remove mistakenly uploaded files

--- test.js
+++
@@ -1,7 +0,0 @@
-import { APKMirrorScraper } from './dist/esm/index.js'
-
-const aps = new APKMirrorScraper({
-    arch: 'arm64-v8a'
-})
-
-console.log(await aps.fetchDownload('google-inc/youtube', '17.35.35'))
